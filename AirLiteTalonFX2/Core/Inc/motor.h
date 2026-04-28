04582cbbaa40acfd825c48fa8e79f0110f2af04b
vqxel <rezq.bagheri@gmail.com>
2026-04-28 12:20:00 -0700

    Added motor support. Untested.

---
+++ AirLiteTalonFX2/Core/Inc/motor.h
@@ -0,0 +1,43 @@
+/*
+ * motor.h
+ *
+ *  Created on: Apr 28, 2026
+ *      Author: rezq
+ */
+
+#ifndef INC_MOTOR_H_
+#define INC_MOTOR_H_
+
+#include "stm32f4xx_hal.h"
+#include "main.h"
+#include "math.h"
+
+typedef enum {
+	FORWARD,
+	BACKWARD,
+	OFF
+} DriveDirection;
+
+typedef struct {
+	GPIO_TypeDef *forwardPort;
+	GPIO_TypeDef *backwardPort;
+	uint16_t forwardPin;
+	uint16_t backwardPin;
+	volatile uint32_t *pwmCcr;
+	TIM_HandleTypeDef *tim;
+	uint32_t timChannel;
+	bool inverted;
+
+	float32_t deadband;
+	DriveDirection lastDriveDirection;
+} Motor;
+
+bool Motor_Init(Motor *motor, PIO_TypeDef *forwardPort, GPIO_TypeDef *backwardPort, uint16_t forwardPin, uint16_t backwardPin, volatile uint32_t *pwmCcr, TIM_HandleTypeDef *tim, uint32_t timChannel, bool inverted);
+
+void Motor_SetDirection(Motor *motor, DriveDirection direction); // TODO: I could make this return a bool but GPIO pin sets don't really fail... lol
+
+void Motor_SetDutyCycle(Motor *motor, float32_t dutyCycle);
+
+void Motor_Set(Motor *motor, float32_t dutyCycle);
+
+#endif /* INC_MOTOR_H_ */
---
+++ AirLiteTalonFX2/Core/Src/motor.c
@@ -0,0 +1,87 @@
+/*
+ * motor.c
+ *
+ *  Created on: Apr 28, 2026
+ *      Author: rezq
+ */
+
+#include "motor.h"
+
+bool Motor_Init(Motor *motor, PIO_TypeDef *forwardPort, GPIO_TypeDef *backwardPort, uint16_t forwardPin, uint16_t backwardPin, volatile uint32_t *pwmCcr, TIM_HandleTypeDef *tim, uint32_t timChannel, bool inverted) {
+	motor->forwardPort = forwardPort;
+	motor->backwardPort = backwardPort;
+	motor->forwardPin = forwardPin;
+	motor->backwardPin = backwardPin;
+	motor->pwmCcr = pwmCcr;
+	motor->tim = tim;
+	motor->timChannel = timChannel;
+	motor->inverted = inverted;
+
+	motor->deadband = 0.01;
+
+	Motor_SetDirection(motor, OFF);
+
+	return HAL_TIM_PWM_Start(motor->tim, motor->timChannel) == HAL_OK;
+}
+
+void Motor_SetDirection(Motor *motor, DriveDirection direction) {
+	GPIO_PinState forwardPinState;
+	GPIO_PinState backwardPinState;
+
+	switch (direction) {
+	case FORWARD:
+		forwardPinState = inverted ? GPIO_PIN_RESET : GPIO_PIN_SET;
+		backwardPinState = inverted ? GPIO_PIN_SET : GPIO_PIN_RESET;
+		break;
+	case BACKWARD:
+		forwardPinState = inverted ? GPIO_PIN_SET : GPIO_PIN_RESET;
+		backwardPinState = inverted ? GPIO_PIN_RESET : GPIO_PIN_SET;
+		break;
+	case OFF:
+		forwardPinState = GPIO_PIN_RESET;
+		backwardPinState = GPIO_PIN_RESET;
+		break;
+	}
+
+	HAL_GPIO_WritePin(motor->forwardPort, motor->forwardPin, forwardPinState);
+	HAL_GPIO_WritePin(motor->backwardPort, motor->backwardPin, backwardPinState);
+
+	motor->lastDriveDirection = direction;
+}
+
+void Motor_SetDutyCycle(Motor *motor, float32_t dutyCycle) {
+	// Clamp duty cycle to legal values TODO: Why was it originally capped at 0.72...
+	if (dutyCycle > 1) {
+		dutyCycle = 1;
+	} else if (dutyCycle < 0) {
+		dutyCycle = 0;
+	}
+
+	int ccr = PWM_ARR * dutyCycle;
+
+	*pwmCcr = ccr;
+}
+
+void Motor_Set(Motor *motor, float32_t dutyCycle) {
+	float32_t absDutyCycle = abs(dutyCycle);
+
+	DriveDirection extractedDirection;
+	if (absDutyCycle < motor->deadband) {
+		extractedDirection = OFF;
+		dutyCycle = 0;
+	} else if (dutyCycle > motor->deadband) {
+		extractedDirection = FORWARD;
+	} else {
+		extractedDirection = BACKWARD;
+	}
+
+	if (extractedDirection != motor->lastDriveDirection) {
+		Motor_SetDirection(motor, extractedDirection);
+	}
+
+	if (absDutyCycle > 1) {
+		dutyCycle = 1;
+	}
+
+	Motor_SetDutyCycle(motor, absDutyCycle);
+}
